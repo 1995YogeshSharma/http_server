df9347d036e13b52492a152db1771a9f378ac25b
1995YogeshSharma <yogesh.sharma@research.iiit.ac.in>
2016-04-08 22:36:59 +0530

    multithreading implemented

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import socket
 import commands 
 import os
 import sys
+import thread
 
 
 RESPONSE = """HTTP/1.1 200 OK
@@ -32,25 +33,23 @@ class Server() :
 			request = client.recv(self.bufsize)
 			print "received request header"
 			print request
-			startLine = request.split('\n')[0]
-			method, path, version = startLine.split()
-			curDir = commands.getoutput('pwd')
-			fileRequested = curDir + path
-			print fileRequested
-			if not os.path.exists(fileRequested) :
-				print "file not found on the server"
-				client.sendall('''HTTP/1.1 404 Not Found
-					Date: Sun, 18 Oct 2012 10:36:20 GMT
-					Server: Apache/2.2.14 (Win32)
-					Content-Length: 230
-					Connection: Closed
-					Content-Type: text/html; charset=iso-8859-1''')
-			else:
-				with open(fileRequested) as f :
-					data = f.read()
-					response = RESPONSE.format(data)
-					client.sendall(response)
-			client.close();
+			thread.start_new_thread(do_work, (client, request,))
+			
+def do_work(client, request):
+	startLine = request.split('\n')[0]
+	method, path, version = startLine.split()
+	curDir = commands.getoutput('pwd')
+	fileRequested = curDir + path
+	print fileRequested
+	if not os.path.exists(fileRequested) :
+		print "file not found on the server"
+		client.sendall('HTTP/1.1 404 Not Found ')
+	else:
+		with open(fileRequested) as f :
+			data = f.read()
+			response = RESPONSE.format(data)
+			client.sendall(response)
+	client.close()
 
 
 
