bfcf12bc561a61527f7de8ad1f4691c2c0d4c736
1995YogeshSharma <yogesh.sharma@research.iiit.ac.in>
2016-04-08 18:12:10 +0530

    basic requirements done, server can be opened from browser and html file(or any) can be served

--- server.py
+++ server.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python
 
-import socket 
+import socket
+import commands 
+import os
+import sys
+
+
+RESPONSE = """HTTP/1.1 200 OK
+
+{}"""
 
 class Server() :
 	def __init__(self, host, port) :
@@ -21,5 +29,29 @@ class Server() :
 			print "waiting for connection"
 			client, caddr = self.socket.accept()
 			print "connected to ", caddr
-			data = client.recv(self.bufsize)
-			print data.split()
+			request = client.recv(self.bufsize)
+			print "received request header"
+			print request
+			startLine = request.split('\n')[0]
+			method, path, version = startLine.split()
+			curDir = commands.getoutput('pwd')
+			fileRequested = curDir + path
+			print fileRequested
+			if not os.path.exists(fileRequested) :
+				print "file not found on the server"
+				client.sendall('''HTTP/1.1 404 Not Found
+					Date: Sun, 18 Oct 2012 10:36:20 GMT
+					Server: Apache/2.2.14 (Win32)
+					Content-Length: 230
+					Connection: Closed
+					Content-Type: text/html; charset=iso-8859-1''')
+			else:
+				with open(fileRequested) as f :
+					data = f.read()
+					response = RESPONSE.format(data)
+					client.sendall(response)
+			client.close();
+
+
+
+
